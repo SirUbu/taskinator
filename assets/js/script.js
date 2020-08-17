86130361f133080b7f96875495838ad253885b30
Sir Ubu <TheSirUbu@gmail.com>
2020-08-16 19:23:29 -0600

    Add prevend default when adding new task.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // new task creation function
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+    event.preventDefault();
     var taskItemEl = document.createElement("li");
     taskItemEl.className = "task-item";
-    taskItemEl.textContent = "This is a new task";
+    taskItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(taskItemEl);
 };
 
 // observe the click event listener behavior specific to the button and run createTaskHandler function
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
