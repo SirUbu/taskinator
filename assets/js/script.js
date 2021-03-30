1cf9e6ff34514240a973f2fbfc4cb9fc41884956
Sir Ubu <TheSirUbu@gmail.com>
2021-03-30 16:59:58 -0600

    Refactored code for separate handler functions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,24 +3,35 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // function to add new task to tasks-to-do
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
     // prevent page refresh by default
     event.preventDefault();
 
     // get form input information and store in variables
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    // package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
 
+    // send as argument to createTaskEl function
+    createTaskEl(taskDataObj);
+}
+
+var createTaskEl = function(taskDataObj) {
     // create list item    
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-
+    
     // crate div to hold info and add to list
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
-
+    
     // add html content to div
-    taskInfoEl.innerHTML = `<h3 class="task-name">${taskNameInput} </h3><span class="task-type">${taskTypeInput} </span>`;
+    taskInfoEl.innerHTML = `<h3 class="task-name">${taskDataObj.name} </h3><span class="task-type">${taskDataObj.type} </span>`;
     
     // add to each respective parent element to add to DOM
     listItemEl.appendChild(taskInfoEl)
@@ -28,4 +39,4 @@ var createTaskHandler = function(event) {
 }
 
 // event listeners
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
