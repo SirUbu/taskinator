469247c0fddc785602d39d8a6774e7f4c5e88b75
Sir Ubu <thesirubu@gmail.com>
2021-03-29 21:07:42 -0600

    Updated button submit and event listener.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 // DOM query selectors
-var buttonEL = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // function to add new task to tasks-to-do
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+    
     var taskItemEl = document.createElement("li");
     taskItemEl.className = "task-item";
     taskItemEl.textContent = "A New Task";
@@ -11,4 +13,4 @@ var createTaskHandler = function() {
 }
 
 // event listeners
-buttonEL.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
